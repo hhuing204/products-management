63a342d67fc6501dfa8331d5e22e9a73b7e4c98e
hhuing204 <huynhboi123@gmail.com>
2025-07-25 16:36:36 +0700

    fix middleware user

--- middlewares/client/user.middleware.js
+++ middlewares/client/user.middleware.js
@@ -1,12 +1,13 @@
 const User = require("../../models/user.model")
 
-module.exports.infoUser = (req, res, next) => {
+module.exports.infoUser = async (req, res, next) => {
     if(req.cookies.tokenUser) {
-        const user = User.findOne({
+        const user = await User.findOne({
             tokenUser: req.cookies.tokenUser,
             deleted: false,
             status: "active"
         }).select("-password")
+
 
         if(user) {
             res.locals.user = user
